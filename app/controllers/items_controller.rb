3c7ba897ae9111ad7a5aa1b80f969dd3a29153ea
yudai1019 <yudaydayo@gmail.com>
2022-07-13 22:21:57 +0900

    newアクションの定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,12 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: :new
   def index
   end
 
   def new
-    @items = Item.new
   end
 
   def create
   end
+
 end
