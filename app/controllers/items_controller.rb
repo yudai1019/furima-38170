20769901d81605aeb146566378beef8d03c02766
yudai1019 <yudaydayo@gmail.com>
2022-07-14 11:27:52 +0900

    保存機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,6 +8,13 @@ class ItemsController < ApplicationController
   end
 
   def create
+    Items.create(params_item)
   end
 
+private
+def params_item
+ params.require(:item).permit(:name,:description_item,:category_id,:state_id,:load_id,:area_id,:date_time_id,:price,:image).merge(user_id: current_user.id)
 end
+
+end
+
