36dd99c04dc36f19a3baf92792d3d855817fd54b
yudai1019 <yudaydayo@gmail.com>
2022-07-18 12:44:59 +0900

    商品一覧機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: :new
   def index
-    @items = Item.all
+    @items = Item.includes(:user).order(created_at: :desc)
   end
 
   def new
