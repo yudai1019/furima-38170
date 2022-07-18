38edd868f26c9226b6345602038a1c2e7c1e4f93
yudai1019 <yudaydayo@gmail.com>
2022-07-18 20:30:11 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: :new
+  before_action :authenticate_user!, only: [:new]
   def index
     @items = Item.includes(:user).order(created_at: :desc)
   end
@@ -21,8 +21,21 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
-  private
+  def edit
+    @item = Item.find(params[:id])
+    redirect_to root_path unless current_user.id == @item.user_id
+  end
 
+  def update
+    @item = Item.find(params[:id])
+   if @item.update(params_item)
+    redirect_to  item_path
+   else
+    render :edit,status: :unprocessable_entity
+   end
+  end
+
+  private
   def params_item
     params.require(:item).permit(:name, :description_item, :category_id, :state_id, :load_id, :area_id, :wait_id, :price,
                                  :image).merge(user_id: current_user.id)
