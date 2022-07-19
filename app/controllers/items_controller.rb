88ed07b2e6226139dc36f8e3337e9ca1dbc7497f
yudai1019 <yudaydayo@gmail.com>
2022-07-19 14:50:16 +0900

    訂正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -35,9 +35,9 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    item = Item.find(params[:id])
+    item = Item.find(params[:id]) unless current_user.id == @item.user_id
     item.destroy
-    redirect_to root_path unless current_user.id == @item.user_id
+    redirect_to root_path 
   end
 
   private
