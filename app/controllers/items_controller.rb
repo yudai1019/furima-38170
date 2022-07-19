1d49c1fe8e72046d3b033b972e0e766978fb2df0
yudai1019 <yudaydayo@gmail.com>
2022-07-19 14:59:15 +0900

    あ

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -35,7 +35,7 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    item = Item.find(params[:id]) unless current_user.id == @item.user_id
+    item = Item.find(params[:id])  unless @item.user_id
     item.destroy
     redirect_to root_path 
   end
