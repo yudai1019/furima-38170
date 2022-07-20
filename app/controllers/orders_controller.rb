36196fb98a1f19dfd80815341c3ad2553e8f79b6
yudai1019 <yudaydayo@gmail.com>
2022-07-20 22:19:56 +0900

    orderコントローラー実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,15 @@
 class OrderController < ApplicationController
-end
+
+  def index
+    @item_address = Itemaddress.new
+  end
+
+  def create
+    Itemaddress.create(item_params)
+  end
+
+  def item_params
+    params.require(:item_address).permit( :name,:description_item , :category_id,:state_id,:load_id, :wait_id,:price,:area_id,:municipality,:addressnum、:buildingname、:phonenum,:order) 
+           .merge(user_id: crrent_user.id)
+  end
+
