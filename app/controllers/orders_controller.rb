0b32834e5d3222eda7987601f553ce081541854b
yudai1019 <yudaydayo@gmail.com>
2022-07-22 22:20:30 +0900

    クレジットカード

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,6 +9,12 @@ class OrdersController < ApplicationController
     @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
+      Payjp.api_key = "sk_test_fa88fa22224682e92984a383"  
+      Payjp::Charge.create(
+        amount: order_params[:price],  
+        card: order_params[:token],   
+        currency: 'jpy'                 
+      )
       @order_address.save
       redirect_to root_path
     else
@@ -16,10 +22,10 @@ class OrdersController < ApplicationController
     end
   end
 
- private
+ private 
 
   def order_params
-    params.require(:order_address).permit(:municipality,:addressnum,:buildingname,:phonenum,:area_id).merge(user_id: current_user.id)
+    params.require(:order_address).permit(:municipality,:addressnum,:buildingname,:phonenum,:area_id,:postnum).merge(user_id: current_user.id , token: params[:token])
   end
 end
 
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,6 +1,6 @@
 class OrderAddress
  include ActiveModel::Model
- attr_accessor :municipality,:addressnum,:buildingname,:phonenum,:order_id,:user_id,:item_id,:area_id, :postnum
+ attr_accessor :municipality,:addressnum,:buildingname,:phonenum,:order_id,:user_id,:item_id,:area_id, :postnum,:token
  
  
  validates :postnum, presence: true,numericality: {with:/\A[0-9]{3}-[0-9]{4}\z/}
@@ -10,7 +10,7 @@ class OrderAddress
  validates :area_id,presence: true,numericality: { other_than: 1 }
  
 
- def save
+ def save               
   order = Order.create(user_id: user_id,item_id: item_id)
   Address.create(postnum: postnum,municipality: municipality,addressnum: addressnum,buildingname: buildingname,phonenum: phonenum,order_id: order.id,area_id: area_id)
  end
