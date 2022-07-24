d9dbf2c73b2b4cb9af70d6f4f0151c83c9862476
yudai1019 <yudaydayo@gmail.com>
2022-07-24 21:52:56 +0900

    修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,5 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: :index
   def index
     @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new
