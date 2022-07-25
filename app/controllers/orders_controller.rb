8f5da80c67364c5881de93984be4334eb7f731b4
yudai1019 <yudaydayo@gmail.com>
2022-07-25 11:17:22 +0900

    訂正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,7 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!, only: :index
   def index
     @item = Item.find(params[:item_id])
+    redirect_to root_path if @item.user_id = current_user.id || !@item.order.nil?
     @order_address = OrderAddress.new
   end
 
