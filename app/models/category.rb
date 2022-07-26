8287a1cb4ac2b73b124cdaa16532883e2363bbaa
yudai1019 <yudaydayo@gmail.com>
2022-07-26 15:29:54 +0900

    修正

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,12 +4,13 @@ class Category < ActiveHash::Base
     { id: 2, category: 'メンズ' },
     { id: 3, category: 'レディース' },
     { id: 4, category: 'ベビー・キッズ' },
-    { id: 5, category: 'インテリア・住まい・小物、本・音楽・ゲーム' },
-    { id: 6, category: 'おもちゃ・ホビー・グッズ' },
-    { id: 7, category: '家電・スマホ・カメラ' },
-    { id: 8, category: 'スポーツ・レジャー' },
-    { id: 9, category: 'ハンドメイド' },
-    { id: 10, category: 'その他' }
+    { id: 5, category: 'インテリア・住まい・小物' },
+    { id: 6, category: '本・音楽・ゲーム'},
+    { id: 7, category: 'おもちゃ・ホビー・グッズ' },
+    { id: 8, category: '家電・スマホ・カメラ' },
+    { id: 9, category: 'スポーツ・レジャー' },
+    { id: 10, category: 'ハンドメイド' },
+    { id: 11, category: 'その他' }
   ]
   include ActiveHash::Associations
   has_many :items
